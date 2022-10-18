a4bcee594fddd34d3f99f3fe7508893c8745546c
JaviRGallegos <93193012+JaviRGallegos@users.noreply.github.com>
2022-10-18 16:10:47 +0200

    Add files via upload

--- Lista_compra.py
+++ Lista_compra.py
@@ -1,7 +1,8 @@
 class Producto(): 
-    dicc={} 
+     
     def __init__(self, cod_articulo: int, nombre: str, descripcion: str, precio: int): 
         #Cojo los atributos y monto sobre la marcha el diccionario que voy a meter en la lista
+        self.dicc = {}
         self.cod_articulo = cod_articulo 
         self.nombre = nombre 
         self.descripcion = descripcion 
@@ -9,36 +10,37 @@ class Producto():
         self.dicc["codigo"]= self.cod_articulo 
         self.dicc["nombre"]= self.nombre 
         self.dicc["descripcion"]= self.descripcion 
-        self.dicc["precio"]= self.precio 
+        self.dicc["precio"]= self.precio
          
- 
+
 class Cesta: 
     #Lista donde voy a almacenar los diccionarios que contienen los datos de la clase Producto
-    cesta_prod = [] 
-     
-    def alta(self, nuevoArticulo): 
-        '''Añadir producto al array cesta_prod'''
-        self.cesta_prod.append(nuevoArticulo.dicc) 
+    def __init__(self) -> None:
+        self.cesta_prod = []
          
+    def alta(self, nuevoProducto): 
+        '''Añadir producto al array cesta_prod'''
+        self.cesta_prod.append(nuevoProducto.dicc)
+
     def listar(self): 
         '''Imprimir todos los articulos que hay en la lista cesta_prod'''
-        for producto in self.cesta_prod: 
+        for producto in self.cesta_prod:
             print("Código:", producto["codigo"], "|", end = ' ') #Quiero imprimir todo en la misma línea, el end por defecto en python vale "\n"
-            print("Nombre:", producto["nombre"], "|", end = ' ') 
-            print("Descripcion:", producto["descripcion"], "|", end = ' ') 
-            print("Precio:", producto["precio"],"\n") 
-         
-    def baja(self,codigo): 
+            print("Nombre:", producto["nombre"], "|", end = ' ')
+            print("Descripcion:", producto["descripcion"], "|", end = ' ')
+            print("Precio:", producto["precio"],"\n")
+
+    def baja(self,codigo):
         '''Quitar un producto del array cesta_prod'''
-        for i in range (len(self.cesta_prod)): 
-            if self.cesta_prod[i]["codigo"] == codigo: 
-                self.cesta_prod.remove(self.cesta_prod[i]) 
+        for i in range (len(self.cesta_prod)):
+            if self.cesta_prod[i]["codigo"] == codigo:
+                self.cesta_prod.remove(self.cesta_prod[i])
                       
-    def modificar(self,codigo_mod): 
+    def modificar(self,codigo_mod):
         '''Modificar un atributo del diccionario, buscado por código del artículo, a elección del usuario'''
         resul = int(input("Introduce qué quieres modificar (1->nombre | 2->descripcion | 3->precio)"))
-        for producto in self.cesta_prod: 
-            if producto["codigo"] == codigo_mod: 
+        for producto in self.cesta_prod:
+            if producto["codigo"] == codigo_mod:
                 if resul == 1:
                     producto["nombre"] = input("Introduce el nuevo nombre del producto: ") 
                 elif resul == 2:
@@ -61,13 +63,60 @@ class Cesta:
                 print("Descripcion", articulo["descripcion"]) 
                 print("Precio", articulo["precio"],"\n") 
      
-#Comprobaciones de cada uno de los métodos 
-nuevoArticulo = Producto(1, "teléfono", "es un teléfono",25) 
-cesta_prod1 = Cesta() 
-cesta_prod1.alta(nuevoArticulo) 
-cesta_prod1.baja(2) 
-#cesta_prod1.modificar(2) 
-# codigo_prod = 2
-# cesta_prod1.buscar(codigo_prod)
-cesta_prod1.listar() 
 
+
+
+cesta_prod1 = Cesta()
+
+# Meto varios artículos para comprobar que funciona
+nuevoArticulo = Producto(1, "teléfono", "es un teléfono",25)
+
+# cesta_prod1.cesta_prod = []
+cesta_prod1.alta(nuevoArticulo)
+nuevoArticulo2 = Producto(2, "ordenador", "es un ordenador",25)
+nuevoArticulo3 = Producto(3, "televisor", "es un televisor",25)
+cesta_prod1.alta(nuevoArticulo3)
+cesta_prod1.listar() # 1, 3
+
+print("========================================================================")
+nuevoArticulo4 = Producto(4, "portátil", "es un portátil",25)
+cesta_prod1.alta(nuevoArticulo2)
+cesta_prod1.alta(nuevoArticulo4)
+
+cesta_prod1.listar() # imprime 1, 3, 2 y 4 (en ese orden)
+#Comprobaciones de cada uno de los métodos 
+#### DESCOMENTAR USANDO CONTROL + Ç ####
+#cesta_prod1.baja(2)
+#cesta_prod1.modificar(3)
+#codigo_prod = 2
+#cesta_prod1.buscar(codigo_prod)
+
+#######################################################################################
+
+
+accion = input("Introduzca acción a realizar (1 -> alta, 2 -> baja, 3 -> modificar, 4-> buscar, 5 -> listar, salir mata el programa)")
+while accion != "salir":
+    if accion == "1":
+        codigo = int(input("Introduzca el código del nuevo artículo: "))
+        nombre = input("Introduzca el nombre del artículo: ")
+        desc = input("Introduzca la descripción del artículo: ")
+        precio = int(input("Introduzca el precio del artículo (no números racionales)"))
+        nuevoProducto = Producto(codigo, nombre, desc, precio)
+        cesta_prod1.alta(nuevoProducto)
+        cesta_prod1.listar()
+        accion = input("Introduzca acción a realizar (1 -> alta, 2 -> baja, 3 -> modificar, 4-> buscar, 5 -> listar, salir mata el programa)")
+    elif accion == "2":
+        cod_baja = int(input("Intruzca código del producto a retirar (solo enteros): "))
+        cesta_prod1.baja(cod_baja)
+        accion = input("Introduzca acción a realizar (1 -> alta, 2 -> baja, 3 -> modificar, 4-> buscar, 5 -> listar, salir mata el programa)")
+    elif accion == "3":
+        cod_modificar = int(input("Intruzca código del producto a modificar (solo enteros): "))
+        cesta_prod1.modificar(cod_modificar)
+        accion = input("Introduzca acción a realizar (1 -> alta, 2 -> baja, 3 -> modificar, 4-> buscar, 5 -> listar, salir mata el programa)")
+    elif accion == "4":
+        cod_buscar = int(input("Intruzca código del producto a buscar (solo enteros): "))
+        cesta_prod1.buscar(cod_buscar)
+        accion = input("Introduzca acción a realizar (1 -> alta, 2 -> baja, 3 -> modificar, 4-> buscar, 5 -> listar, salir mata el programa)")
+    elif accion == "5":
+        cesta_prod1.listar()
+        accion = input("Introduzca acción a realizar (1 -> alta, 2 -> baja, 3 -> modificar, 4-> buscar, 5 -> listar, salir mata el programa)")
